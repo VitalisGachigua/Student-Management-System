c7e4fb349ca9c925b56433485c391cf8ba726284
Vitalis Gachigua <matherivitalis@gmail.com>
2026-07-14 20:21:46 +0300

    Initialization of StudentMS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+  
+     <h1 classname="text-4xl font-bold text-center mt-10">
+      Student Management System
+     </h1>
+    
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import {Link} from "react-router-dom";
+
+function Navbar() {
+
+    return (
+        <nav className="bg-indigo-600 text-white shadow-md">
+            <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+                <h1 className ="text-2xl font-bold">
+                    Student Management <System></System>
+                </h1>
+                <div className="flex gap-6">
+                    <Link to="/">Home</Link>
+                    <Link to="/students"></Link>
+                    <Link to="/add-students">Students</Link>
+                    <Link to="/dashboard">Dashboard</Link>
+                    <Link to="/about">About</Link>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
